693c2a80758e9e6cc8ff6f667f47e57721d3249a
vanderson <vanderson.moraes14@gmail.com>
2026-09-14 14:40:39 -0400

    update menu-hamburguer

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+const btnMenu = document.getElementById("menu-hamburguer");
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu-link");
 
+btnMenu.addEventListener("click", () => {
+    menu.classList.toggle("ativo");
+});
 
-const btnMenu = document.getElementById("menu-hamburguer")
-const menu = document.getElementById("menu")
-
-btnMenu.addEventListener('click', () => {
-    menu.classList.toggle('ativo')
-})
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        links.forEach(item => item.classList.remove("ativo"));
+        link.classList.add("ativo");
+    });
+});
